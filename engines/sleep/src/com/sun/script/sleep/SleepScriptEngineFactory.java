63d42f7202fe6dab47059c4c9c8a6d859623a342
sundararajana <sundararajana@03d6dbfd-e0f2-6d84-cd2b-8a81ba2ddabe>
2006-05-01 03:21:51 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    Fixed bug in getOutputStatement.
    
    
    git-svn-id: https://svn.java.net/svn/scripting~svn/trunk@13 03d6dbfd-e0f2-6d84-cd2b-8a81ba2ddabe

--- engines/sleep/src/com/sun/script/sleep/SleepScriptEngineFactory.java
+++ engines/sleep/src/com/sun/script/sleep/SleepScriptEngineFactory.java
@@ -80,8 +80,26 @@ public class SleepScriptEngineFactory implements ScriptEngineFactory {
         return names;
     }
 
-    public String getOutputStatement(String str) {
-        return "print(" + str + ");";
+    public String getOutputStatement(String toDisplay) {
+        StringBuffer buf = new StringBuffer();
+        buf.append("print(\"");
+        int len = toDisplay.length();
+        for (int i = 0; i < len; i++) {
+            char ch = toDisplay.charAt(i);
+            switch (ch) {
+            case '"':
+                buf.append("\\\"");
+                break;
+            case '\\':
+                buf.append("\\\\");
+                break;
+            default:
+                buf.append(ch);
+                break;
+            }
+        }
+        buf.append("\"");
+        return buf.toString();
     }
 
     public String getParameter(String key) {
