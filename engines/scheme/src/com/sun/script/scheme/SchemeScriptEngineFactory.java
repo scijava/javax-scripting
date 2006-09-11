7a800fbab4986994d04522d70c25d0a426497c44
sundararajana <sundararajana@03d6dbfd-e0f2-6d84-cd2b-8a81ba2ddabe>
2006-09-11 09:40:52 +0000

    Using SISC version 1.15.3
    git-svn-id: https://svn.java.net/svn/scripting~svn/trunk@105 03d6dbfd-e0f2-6d84-cd2b-8a81ba2ddabe

--- engines/scheme/src/com/sun/script/scheme/SchemeScriptEngineFactory.java
+++ engines/scheme/src/com/sun/script/scheme/SchemeScriptEngineFactory.java
@@ -38,7 +38,7 @@ public class SchemeScriptEngineFactory implements ScriptEngineFactory {
     }
 
     public String getEngineVersion() {
-        return "1.15.2";
+        return "1.15.3";
     }
 
     public List<String> getExtensions() {
